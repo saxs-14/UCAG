4c9729ed03673dc855d9c9be42aa4d699b08c229
Phathutshedzo Mamagau <mamagauphathutshedzosaxs@gmail.com>
2026-09-23 20:54:35 +0200

    UX: remove redundant homepage wrapper spacing

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,8 @@ export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
-    <main id="main-content" className="flex flex-1 flex-col items-center bg-paper">
-      <div className="mx-auto flex w-full max-w-5xl flex-col items-center gap-6 p-6 sm:p-8">
-        <CalculatorPage />
-      </div>
+    <main id="main-content" className="flex flex-1 flex-col bg-paper">
+      <CalculatorPage />
     </main>
   );
 }
